9793262e624b4e668a53c6c7bfbc923748f49071
Santiago Arboleda <santiago.arboledavahos19@gmail.com>
2025-10-22 21:09:26 -0500

    Agregar archivos de proyecto.

---
+++ API.W.Movies/DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.W.Movies.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.W.Movies.DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //Sección para crear el dbset de las entidades o modelos
+        public DbSet<Category> Categories { get; set; }
+    }
+}
