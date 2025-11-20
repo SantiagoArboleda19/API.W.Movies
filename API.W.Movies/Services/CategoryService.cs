72f7fc9eccdd7aaa35bdc0dd845af8f75356dd7a
SantiagoArboleda19h <santiago.arboledavahos19@gmail.com>
2025-11-20 09:16:50 -0500

    New CategoriesController and CategoryServices created

---
+++ API.W.Movies/Services/CategoryService.cs
@@ -0,0 +1,55 @@
+﻿using API.W.Movies.DataAccessLayer.Models;
+using API.W.Movies.DataAccessLayer.Models.Dtos;
+using API.W.Movies.Services.IServices;
+using AutoMapper;
+
+namespace API.W.Movies.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryService _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(ICategoryService categoryRepository, IMapper mapper) 
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public Task<bool> CategoryExistByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<CategoryDto>> GetCategoriesAsync()
+        {
+            var categories = _categoryRepository.GetCategoriesAsync(); //Solo estoy haciendo el llamado del metodo desde la capa de repository
+
+            return _mapper.Map<ICollection<CategoryDto>>(categories); //Mapeo la lista de categorias a una lista de categorias Dto
+        }
+
+        public Task<Category> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API.W.Movies/Services/IServices/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using API.W.Movies.DataAccessLayer.Models;
+using API.W.Movies.DataAccessLayer.Models.Dtos;
+
+namespace API.W.Movies.Services.IServices
+{
+    public interface ICategoryService
+    {
+        Task<ICollection<CategoryDto>> GetCategoriesAsync();
+        Task<CategoryDto> GetCategoryAsync(int id);
+        Task<bool> CategoryExistByIdAsync(int id);
+        Task<bool> CategoryExistsByNameAsync(string name);
+        Task<bool> CreateCategoryAsync(Category category);
+        Task<bool> UpdateCategoryAsync(Category category);
+        Task<bool> DeleteCategoryAsync(int id);
+    }
+}
